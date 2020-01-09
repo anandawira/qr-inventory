647fb67f66c1e73985d56ad1b67d82acf1ec9c9b
AnandaWira <anandawira754@gmail.com>
2020-01-09 22:15:24 +0700

    Initial commit

---
+++ app/src/main/java/com/ananda/qrinventory/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ananda.qrinventory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ananda.qrinventory.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/ananda/qrinventory/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.ananda.qrinventory.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
